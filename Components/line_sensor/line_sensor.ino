f730d2143676b183f30d7b6047da151b888ae79c
wreevehicks <wrhicks@bu.edu>
2015-10-09 10:15:57 -0400

    Added comments to line_sensor, created simple ultrasonic test code

--- Components/line_sensor/line_sensor.ino
+++ Components/line_sensor/line_sensor.ino
@@ -1,5 +1,10 @@
-#include "LineSensor.h"
-LineSensor sense(A0, A1, A2);
+// This script sets up your line sensor and lets you see the outputs from it. 
+
+#include "LineSensor.h" // this includes the library we made for the line sensor
+
+// These are the pins that the line sensor uses, follow the instructions in the line-sensor 
+// set up to make sure they are connected properly.
+LineSensor sense(A0, A1, A2); 
 
 void setup() {
   Serial.begin(9600);
@@ -12,3 +17,7 @@ void loop() {
   if(sense.isLineVisible()) Serial.println(sense.getLinePosition());
   delay(20);
 }
+// now move your connected line sensor over a dark line and read the 
+// serial output (Click the magnifying glass looking button in the top
+// right corner of this box).
+
